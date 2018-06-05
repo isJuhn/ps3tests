0562316e87837715f1775e05ab3d9e3727b9c1d3
isJuhn <devil0129@gmail.com>
2018-06-05 20:42:45 +0200

    Added more stuff

--- sys_vm_append_memory/sys_vm_append_memory.cpp
+++ sys_vm_append_memory/sys_vm_append_memory.cpp
@@ -22,22 +22,58 @@ typedef uint8_t u8;
 typedef int32_t s32;
 
 sys_addr_t addr;
+sys_addr_t addr2;
 
 int main() {
 
 	int result;
 	
-	printf("sample started.\n");
+	printf("Sample started.\n");
 	
-	result = sys_vm_memory_map(0x10000000, 0x100000, 0xffffffff, 0x200, 0x1, &addr);
-	printf("sys_vm_memory_map(0x10000000,...) returned 0x%x\n", result);
+	printf("Mapping 32MB of virtual memory with 1MB of physical memory used.\n");
+	result = sys_vm_memory_map(0x2000000, 0x100000, 0xffffffff, 0x200, 0x1, &addr);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_memory_map(0x2000000,...) returned 0x%x\n", result);
+		return 0;
+	}
+	printf("Address: 0x%x\n", addr);
 	
+	printf("Mapping 32MB of virtual memory with 1MB of physical memory used.\n");
+	result = sys_vm_memory_map(0x2000000, 0x100000, 0xffffffff, 0x200, 0x1, &addr2);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_memory_map(0x2000000,...) returned 0x%x\n", result);
+		return 0;
+	}
+	printf("Address: 0x%x\n", addr2);
+	
+	printf("Unmapping second map.\n");
+	result = sys_vm_unmap(addr2);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_unmap() returned 0x%x\n", result);
+		return 0;
+	}
+	
+	sys_vm_statistics_t stats;
+	result = sys_vm_get_statistics(addr, &stats);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_get_statistics() returned 0x%x\n", result);
+		return 0;
+	}
+	printf("sys_vm_get_statistics:\n");
+	printf("pmem_total: 0x%x\n", stats.pmem_total);
+	printf("pmem_used: 0x%x\n", stats.pmem_used);
+	
+	printf("Appending 512MB in 1MB chunks.\n");
 	for (int i = 0; i < 0x200; i++)
 	{
 		result = sys_vm_append_memory(addr, 0x100000);
 		if (result != 0)
 		{
-			printf("sys_vm_append_memory(..., 0x100000) returned 0x%x on iteration %d\n", result, i);
+			printf("ERROR: sys_vm_append_memory(..., 0x100000) returned 0x%x on iteration %d\n", result, i);
 			break;
 		}
 	}
@@ -47,7 +83,69 @@ int main() {
 		printf("sys_vm_append_memory(..., 0x100000) never returned an error after 512 iterations\n");
 	}
 	
-    printf("sample finished.\n");
+	result = sys_vm_get_statistics(addr, &stats);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_get_statistics() returned 0x%x\n", result);
+		return 0;
+	}
+	printf("sys_vm_get_statistics:\n");
+	printf("pmem_total: 0x%x\n", stats.pmem_total);
+	printf("pmem_used: 0x%x\n", stats.pmem_used);
+	
+	printf("Unmapping.\n");
+	result = sys_vm_unmap(addr);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_unmap() returned 0x%x\n", result);
+		return 0;
+	}
+	
+	printf("Mapping 256MB of virtual memory with 1MB of physical memory used.\n");
+	result = sys_vm_memory_map(0x10000000, 0x100000, 0xffffffff, 0x200, 0x1, &addr);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_memory_map(0x10000000,...) returned 0x%x\n", result);
+		return 0;
+	}
+	
+	result = sys_vm_get_statistics(addr, &stats);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_get_statistics() returned 0x%x\n", result);
+		return 0;
+	}
+	printf("sys_vm_get_statistics:\n");
+	printf("pmem_total: 0x%x\n", stats.pmem_total);
+	printf("pmem_used: 0x%x\n", stats.pmem_used);
+	
+	printf("Appending 512MB in 1MB chunks.\n");
+	for (int i = 0; i < 0x200; i++)
+	{
+		result = sys_vm_append_memory(addr, 0x100000);
+		if (result != 0)
+		{
+			printf("ERROR: sys_vm_append_memory(..., 0x100000) returned 0x%x on iteration %d\n", result, i);
+			break;
+		}
+	}
+	
+	if (result == 0)
+	{
+		printf("sys_vm_append_memory(..., 0x100000) never returned an error after 512 iterations\n");
+	}
+	
+	result = sys_vm_get_statistics(addr, &stats);
+	if (result != 0)
+	{
+		printf("ERROR: sys_vm_get_statistics() returned 0x%x\n", result);
+		return 0;
+	}
+	printf("sys_vm_get_statistics:\n");
+	printf("pmem_total: 0x%x\n", stats.pmem_total);
+	printf("pmem_used: 0x%x\n", stats.pmem_used);
+	
+    printf("Sample finished.\n");
 
     return 0;
 }
